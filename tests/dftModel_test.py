36a2585be28cd9e3e3fdf1626fc5b9e3cec4f9ad
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2015-12-28 23:09:09 +0100

    #3 Completely refactor the DFT model.
    - use more descriptive names instead of short mathematical symbols so that it is understandable even for beginners
    - split the code into a lot of small functions
    - implement reconstruct() using from_audio() and to_audio() to remove duplication
    - fix the test and cover also the reconstruct() function

--- tests/dftModel_test.py
+++ tests/dftModel_test.py
@@ -9,10 +9,15 @@ def test_simple_sinusoid():
     t = np.linspace(0, 1, window_size)
     x = np.cos(4 * 2 * np.pi * t)
     window = get_window('hamming', window_size)
+    window /= sum(window)
     mag_spectrum, phase_spectrum = dft.from_audio(x, window, window_size)
-    x_reconstructed = dft.to_audio(mag_spectrum, phase_spectrum, window_size) * sum(window)
+    x_reconstructed = dft.to_audio(mag_spectrum, phase_spectrum, window_size)
 
     assert mag_spectrum.argmax() == 4
     assert round(mag_spectrum.max()) == -6
     assert round(mag_spectrum.mean()) == -147
     assert np.allclose(x_reconstructed, x * window)
+
+    x_reconstructed_by_model = dft.reconstruct(x, window, window_size)
+
+    assert np.allclose(x_reconstructed_by_model, x * window)
